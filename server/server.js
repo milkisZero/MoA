6f2ef863b7531ac39f4152800e33c7db61aa9c9b
milkisZero <mjk3482@gmail.com>
2024-11-25 20:37:27 +0900

    feat: view club event api

--- server/server.js
+++ server/server.js
@@ -170,6 +170,27 @@ app.get('/api/club/:postId', async (req, res) => {
 });
 
 // 동아리 일정보기
+app.get('/api/club/:clubId/event', async (req, res) => {
+    try {
+        const clubId = req.params.clubId;
+        const { year, month } = req.query;
+
+        const foundEvents = await Event.find({
+            clubId: clubId,
+            date: {
+                $gte: new Date(`${year}-${month}-01`),
+                $lt: new Date(month === '12' ? `${year + '1'}-${month}-01` : `${year}-${month + '1'}-01`),
+            },
+        });
+
+        return res.status(200).json({
+            success: true,
+            foundEvents,
+        });
+    } catch (err) {
+        return res.status(400).json({ success: false, err });
+    }
+});
 
 // 마이페이지 - 일정보기
 
