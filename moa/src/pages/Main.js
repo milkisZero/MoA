43328b3b9cc7eba2c878d5ea099f64fc0dd46f57
milkisZero <mjk3482@gmail.com>
2024-11-27 00:53:35 +0900

    feat: main - basic footer

--- moa/src/pages/Main.js
+++ moa/src/pages/Main.js
@@ -13,6 +13,7 @@ function Main() {
             </section>
             <MainClubs />
             <ListSection />
+            <Footer />
         </div>
     );
 }
@@ -75,6 +76,18 @@ function ListSection() {
                 ))}
             </div>
         </section>
+    );
+}
+
+function Footer() {
+    return (
+        <footer className="Footer">
+            <div>
+                <h4>개인정보 | 대학정보공시 | 사이트맵</h4>
+                <p>16499 경기도 수원시 영통구 월드컵로 206 아주대학교 T. 031-219-2114</p>
+                <p>Copyright © 2020 Ajou University. All Rights Reserved.</p>
+            </div>
+        </footer>
     );
 }
 
