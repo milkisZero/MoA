767ed21784f8e9d45885dd8819dc2f3a77ec2154
장명현 <gabang002@gmail.com>
2024-12-03 17:27:03 +0900

    [FEAT] user의 msgRooms에 Id 추가 작업 및 테스트 완료

--- server/router/club.js
+++ server/router/club.js
@@ -53,15 +53,15 @@ router.post('/', upload.single('img'), async (req, res) => {
         });
 
         newClub.msgRoomId = newMsgRoom._id;
-        await newClub.save();
-        await newMsgRoom.save();
-
-        const clubId = newClub._id;
-        const user = await User.findById(members);
-        if (user && !user.clubs.includes(clubId)) {
-            user.clubs.push(clubId);
-            await user.save();
-        }
+        
+        await Promise.all([newClub.save(), newMsgRoom.save()]);
+        await User.updateMany(
+            { _id: { $in: members }},
+            { $addToSet: { 
+                    clubs: newClub._id, 
+                    msgRooms: newMsgRoom._id,
+            }},
+        );
 
         res.status(200).json({
             message: 'Club and Clubchatroom created successfully',
@@ -81,15 +81,20 @@ router.get('/total_club', async (req, res) => {
     try {
         const { page, limit } = req.query;
 
-        const club = await Club.find()
-            .sort({ members: -1 })
-            .skip((page - 1) * limit)
-            .limit(Number(limit));
-        if (!club) return res.status(404).json({ message: 'cannot found club list' });
+        const [totalClubs, club] = await Promise.all([
+            Club.countDocuments(),
+            Club.find()
+                .sort({ members: -1 })
+                .skip((page - 1) * limit)
+                .limit(Number(limit)),
+        ]);
+        if (!club || totalClubs === 0)
+            return res.status(404).json({ message: 'cannot found club list' });
 
         return res.status(200).json({
             message: `조회수 ${(page - 1) * limit + 1}위 부터 ${limit}개 list`,
             club,
+            totalClubs
         });
     } catch (e) {
         console.log('get error in /club/total_club: ', e);
@@ -148,24 +153,21 @@ router.put('/members/:clubId', async (req, res) => {
     try {
         const clubId = req.params.clubId;
         const { members } = req.body;
-        const addMembers = members;
 
         const club = await Club.findById(clubId);
         if (!club) return res.status(404).json({ message: 'Club not found' });
 
-        const newMembers = addMembers.filter((member) => !club.members.includes(member));
+        const newMembers = members.filter((member) => !club.members.includes(member));
         club.members.push(...newMembers);
         await club.save();
 
-        const updatedMembers = newMembers.map(async (userId) => {
-            const user = await User.findById(userId);
-
-            if (user && !user.clubs.includes(clubId)) {
-                user.clubs.push(clubId);
-                await user.save();
-            }
-        });
-        await Promise.all(updatedMembers);
+        await User.updateMany(
+            { _id: { $in: newMembers } },
+            { $addToSet: { 
+                    clubs: clubId, 
+                    msgRooms: club.msgRoomId 
+            }},
+        );
 
         return res.status(200).json({
             message: 'Members updated successfully',
@@ -189,17 +191,15 @@ router.delete('/members/:clubId', async (req, res) => {
         const deleteMembers = new Set(members.map(String));
         club.members = club.members.filter((member) => !deleteMembers.has(member.toString()));
         club.admin = club.admin.filter((member => !deleteMembers.has(member.toString())));
-
-        const updatedMembers = members.map(async (userId) => {
-            const user = await User.findById(userId);
-            const idx = user.clubs.indexOf(clubId);
-            if (idx > -1) {
-                user.clubs.splice(idx, 1);
-                await user.save();
-            }
-        });
-
-        await Promise.all([club.save(), ...updatedMembers]);
+        
+        await club.save();
+        await User.updateMany(
+            { _id: { $in: members } },
+            { $pull: {
+                    clubs: clubId,
+                    msgRooms: club.msgRoomId,
+            }},
+        );
 
         return res.status(200).json({
             message: 'Members removed successfully',
@@ -239,12 +239,12 @@ router.delete('/admin/:clubId', async (req, res) => {
     try {
         const clubId = req.params.clubId;
         const { admin } = req.body;
-        const deleteAdmin = admin;
 
         const club = await Club.findById(clubId);
         if (!club) return res.status(404).json({ message: 'Club not found' });
 
-        club.admin = club.admin.filter((admin) => !deleteAdmin.includes(admin.toString()));
+        const deleteAdminSet = new Set(admin.map(String));
+        club.admin = club.admin.filter(adminId => !deleteAdminSet.has(adminId.toString()));
         await club.save();
 
         return res.status(200).json({
@@ -264,15 +264,17 @@ router.delete('/:clubId', async (req, res) => {
         if (!club) return res.status(404).json({ message: 'cannot found Club' });
 
         const clubId = club._id;
-        const deletedMemebers = club.members.map(async (userId) => {
-            const user = await User.findById(userId);
+        const msgRoomId = club.msgRoomId;
 
-            if (user && !user.clubs.includes(clubId)) {
-                user.clubs.push(clubId);
-                await user.save();
-            }
-        });
-        await Promise.all(deletedMemebers);
+        await User.updateMany(
+            { _id: { $in: club.members } },
+            { $pull: { 
+                clubs: clubId, 
+                msgRooms: msgRoomId, 
+            }}
+        );
+
+        await MsgRoom.findByIdAndDelete(msgRoomId);
 
         return res.status(200).json({
             message: 'Club successfully deleted',
@@ -287,29 +289,31 @@ router.delete('/:clubId', async (req, res) => {
 // 동아리 가입 신청
 router.post('/proposer/:clubId', async (req, res) => {
     try {
-        const userId = req.body.userId;
+        const { userId, aprove } = req.body;
         const clubId = req.params.clubId;
 
-        const user = await User.findById(userId);
+        const [user, club] = await Promise.all([User.findById(userId), Club.findById(clubId)]);
         if (!user) return res.status(404).json({ message: 'User cannot found' });
-
-        const club = await Club.findById(clubId);
         if (!club) return res.status(404).json({ message: 'Club cannot found' });
 
-        if (!user.waitingClubs.includes(clubId)) {
-            user.waitingClubs.push(clubId);
-            await user.save();
-        }
+        const updateUserPromise = !user.waitingClubs.includes(clubId)
+            ? User.updateOne(
+                  { _id: userId },
+                  { $addToSet: { waitingClubs: clubId } }
+              )
+            : null;
 
-        if (!club.proposers.includes(userId)) {
-            club.proposers.push(userId);
-            await club.save();
-        }
+        const updateClubPromise = !club.proposers.includes(userId)
+            ? Club.updateOne(
+                  { _id: clubId },
+                  { $addToSet: { proposers: userId } }
+              )
+            : null;
+
+        await Promise.all([updateUserPromise, updateClubPromise]);
 
         res.status(200).json({
             message: 'Successfully proposered',
-            user,
-            club,
         });
     } catch (e) {
         console.log('post error in /club/proposer/:clubId: ', e);
@@ -324,29 +328,29 @@ router.post('/approve/:clubId', async (req, res) => {
         const { userId, approve } = req.body;
         const clubId = req.params.clubId;
 
-        const user = await User.findById(userId);
-        const club = await Club.findById(clubId);
+        const [user, club] = await Promise.all([User.findById(userId), Club.findById(clubId)]);
         if (!user) return res.status(404).json({ message: 'User cannot found' });
         if (!club) return res.status(404).json({ message: 'Club cannot found' });
 
-        const userIdx = user.waitingClubs.indexOf(clubId);
-        if (userIdx > -1) user.waitingClubs.splice(userIdx, 1);
+        const updateUser = {
+            $pull: { waitingClubs: clubId },
+            ...(approve && { $addToSet: { clubs: clubId, msgRooms: club.msgRoomId } }),
+        };
 
-        const clubIdx = club.proposers.indexOf(userId);
-        if (clubIdx > -1) club.proposers.splice(clubIdx, 1);
+        const updateClub = {
+            $pull: { proposers: userId },
+            ...(approve && { $addToSet: { members: userId } }),
+        };
 
-        if (approve) {
-            user.clubs.push(clubId);
-            club.members.push(userId);
-        }
-
-        await user.save();
-        await club.save();
+        await Promise.all([
+            User.updateOne({ _id: userId }, updateUser),
+            Club.updateOne({ _id: clubId }, updateClub),
+        ]);
 
         res.status(200).json({
             message: approve ? 'Successfully approved' : 'Successfully rejected',
-            user,
-            club,
+            updateUser,
+            updateClub
         });
     } catch (e) {
         console.log('post error in /club/approve/:clubId: ', e);
